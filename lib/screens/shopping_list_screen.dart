17221ce0f3a9ed2cbef6faaf276c63a416cb1944
JosephEduard <josepheduardulyloni_2327250080@mhs.mdp.ac.id>
2025-03-20 10:54:19 +0700

    last touch

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -21,71 +21,74 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Daftar Belanja')),
-      body: Column(
-        children: [
-          // Input Field dan Tombol Tambah
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      hintText: 'Masukkan nama barang',
+      appBar: AppBar(centerTitle: true, title: const Text('Daftar Belanja')),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            // Input Field dan Tombol Tambah
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        hintText: 'Masukkan nama barang',
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    if (_controller.text.isNotEmpty) {
-                      _shoppingService.addShoppingItem(_controller.text);
-                      _controller.clear();
-                    }
-                  },
-                ),
-              ],
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      if (_controller.text.isNotEmpty) {
+                        _shoppingService.addShoppingItem(_controller.text);
+                        _controller.clear();
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-          // Daftar Belanja dengan StreamBuilder
-          Expanded(
-            child: StreamBuilder<Map<String, String>>(
-              stream: _shoppingService.getShoppingList(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return Center(child: Text("Error: ${snapshot.error}"));
-                }
-                if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text("Belum ada item"));
-                }
+            // Daftar Belanja dengan StreamBuilder
+            Expanded(
+              child: StreamBuilder<Map<String, String>>(
+                stream: _shoppingService.getShoppingList(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("Belum ada item"));
+                  }
 
-                final items = snapshot.data!;
-                return ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    final key = items.keys.elementAt(index);
-                    final item = items[key];
+                  final items = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final key = items.keys.elementAt(index);
+                      final item = items[key];
 
-                    return ListTile(
-                      title: Text(item!),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          _shoppingService.removeShoppingItem(key);
-                        },
-                      ),
-                    );
-                  },
-                );
-              },
+                      return ListTile(
+                        title: Text(item!),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            _shoppingService.removeShoppingItem(key);
+                          },
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
